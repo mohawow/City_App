6c314fc7297c8ac8ed5368694502400f25319324
Mohammed Mahbashi <mohawow@gmail.com>
2018-09-16 01:37:38 -0700

    Affordable tech cities

---
+++ cityCodes.js
@@ -0,0 +1,102 @@
+var cityNamesWithCodes=[
+    {
+        "City": "Huntsville, AL",
+        "Code": 113,
+        "image":'https://amp.businessinsider.com/images/5a9d9a06b2ef0c3d018b45d6-1136-853.jpg'
+    },
+    {
+        "City": "Thousand Oaks, CA",
+        "Code": 320,
+        "image":'https://amp.businessinsider.com/images/5a9d9968870413fb008b45b6-1136-852.jpg'
+    },
+    {
+        "City": "Phoenix, AZ",
+        "Code": 4,
+        "image":'https://amp.businessinsider.com/images/5a9d98edd075cf1e008b45ff-1136-852.jpg'
+    },
+    {
+        "City": "Kansas City, MO",
+        "Code": 11738,
+        "image":'https://amp.businessinsider.com/images/5a9d981ccf2f5c1d008b45f6-1334-1000.jpg'
+    },
+    {
+        "City": "Orlando, FL",
+        "Code": 11751,
+        "image":'https://amp.businessinsider.com/images/5a9d9708ed1ed829008b45cd-960-720.jpg'
+    },
+    {
+        "City": "Salt Lake City, UT",
+        "Code": 11768,
+        "image":'https://amp.businessinsider.com/images/5a9d95ce7dfd8f30008b45e6-1136-852.jpg'
+    },
+    {
+        "City": "Nashville, TN",
+        "Code": 19,
+        "image":'https://amp.businessinsider.com/images/5a9d957002957d1c008b460c-1334-1000.jpg'
+    },
+    {
+        "City": "Jacksonville, FL",
+        "Code": 11732,
+        "image":'https://amp.businessinsider.com/images/5a9d957002957d1c008b460c-1334-1000.jpg'
+    },
+    {
+        "City": "Albany, NY",
+        "Code": 226,
+        "image":'https://amp.businessinsider.com/images/5a9d9513b2ef0c3c018b45ae-1136-852.jpg'
+    },
+    {
+        "City": "Baltimore, MD",
+        "Code": 16,
+        "image":'https://amp.businessinsider.com/images/5a9d947fe6a9cf24008b45e1-750-563.jpg'
+    },
+    {
+        "City": "Cincinnati, OH",
+        "Code": 42,
+        "image":'https://amp.businessinsider.com/images/5a9d93ebb2ef0c1f008b45d6-750-563.jpg'
+    },
+    {
+        "City": "Cleveland, OH",
+        "Code": 33,
+        "image":'https://amp.businessinsider.com/images/5a9d938e87041319008b45df-750-563.jpg'
+    },
+    {
+        "City": "Indianapolis, IN",
+        "Code": 11733,
+        "image":'https://amp.businessinsider.com/images/5a9d933eb2ef0c3d018b45b1-750-563.jpg'
+    },
+    {
+        "City": "Columbus, OH",
+        "Code": 11,
+        "image":'https://amp.businessinsider.com/images/5a9d92d5b7efe12e008b45e3-750-563.jpg'
+    },
+    {
+        "City": "Denver, CO",
+        "Code": 17,
+        "image":'https://amp.businessinsider.com/images/5a9d91f6cf2f5c1d008b45d6-750-562.jpg'
+    },
+    {
+        "City": "Detroit, MI",
+        "Code": 11735,
+        "image":'https://amp.businessinsider.com/images/5a9d977202957d27008b45f7-750-563.jpg'
+    },
+    {
+        "City": "Tampa, FL",
+        "Code": 11744,
+        "image":'https://amp.businessinsider.com/images/5a9d83c9b2ef0c1d008b4590-750-481.jpg'
+    },
+    {
+        "City": "Providence, RI",
+        "Code": 1507,
+        "image":'https://amp.businessinsider.com/images/5a9d8338cf2f5c2d008b459e-750-562.jpg'
+    },
+    {
+        "City": "Seattle, WA",
+        "Code": 15,
+        "image":'https://amp.businessinsider.com/images/5a9d82c8b2ef0c55008b4595-750-562.jpg'
+    },
+    {
+        "City": "Chicago, IL",
+        "Code": 12390,
+        "image":'https://amp.businessinsider.com/images/5a9d826ccf2f5c1c008b4591-750-563.jpg'
+    }
+];
---
+++ main.js
@@ -0,0 +1,146 @@
+const NUMBEO_URL = 'https://cors-anywhere.herokuapp.com/https://www.numbeo.com/api/city_prices?api_key=sx4z80hb0fuss2&query=';
+var dataset1=[],dataset2=[], cityNameOne, cityNameTwo,detailsCostofLiving=[],i=0,j=0, cityImageOne,cityImageTwo;
+
+function getCityName(){
+    $.each(cityNamesWithCodes, function (index, value) {
+        $('#cityNameOne').append('<option value="' + value.City + '">');
+        $('#cityNameTWo').append('<option value="' + value.City + '">');
+    });
+};
+getCityName();
+function getCostOfLivingApiData(cityName,callBack){
+    $.getJSON(NUMBEO_URL + cityName, callBack);
+};
+function filterData(result,cityName) {
+    let totalcostofliving=0;
+    cityNameArray=result.name.toString().split(',');
+    cityName=cityNameArray[0]+','+cityNameArray[1];
+    $.each(result.prices, function( index, value ) {
+        if (value.item_id==2 || value.item_id==26 || value.item_id==30) {
+            totalcostofliving=Math.round(totalcostofliving+value.average_price);
+            detailsCostofLiving.push(Math.round(value.average_price));
+        }
+    });
+    $('#result1 .textBottom').append(dispalyResultDom(totalcostofliving,cityName));
+    dataset1[i]=totalcostofliving;
+    i++;
+};
+function dispalyResultDom(totalcostofliving, cityName) {
+    return `
+		<p> ${cityName} : $<span>${totalcostofliving}</span></p>
+	`
+};
+function getHousePricessApiData(cityCode,callBack){
+    $.getJSON('https://cors.io/?https://www.quandl.com/api/v3/datasets/ZILLOW/C'+cityCode+'_ZRISFRR?api_key=hzmgKTSmvyCcncCbDGtd',callBack);
+};
+function filterData2(result) {
+    let cityName= result.dataset.name.toString().split('-')[3];
+    let cityData= result.dataset.data[0][1];
+    $('#result2 .textBottom').append(dispalyResultDom2(cityName, cityData));
+    dataset2[j]=cityData;
+    j++;
+};
+function dispalyResultDom2(cityName, cityData) {
+    return `
+		<p> ${cityName} : $<span>${cityData}</span></p>
+	`
+};
+function formSubmission() {
+    $('#form').submit(function(e) {
+        e.preventDefault();
+        $.ajaxSetup({
+            async: false
+        });
+        $('#result1 .textBottom').html('');
+        $('#result2 .textBottom').html('');
+        cityNameOne= $('.cityNameOne').val();
+        cityNameTwo= $('.cityNameTwo').val();
+        if ( cityNameOne.length === 0 || cityNameTwo.length === 0 ){
+            alert('Sorry! You can not compare an empty field. Please try again!');
+            return cityNameOne;
+        };
+        $('.cityNameOne').val('');
+        $('.cityNameTwo').val('');
+        i=0,j=0;
+        cityCodeOne=0;
+        cityCodeTwo=0
+        $.each(cityNamesWithCodes, function( index, value ) {
+            if (cityNameOne===value.City) {cityCodeOne=value.Code; cityImageOne=value.image;}
+            if (cityNameTwo===value.City) {cityCodeTwo=value.Code; cityImageTwo=value.image;}
+        });
+        $('#result1 img').attr("src", cityImageOne);
+        $('#result2 img').attr("src", cityImageTwo);
+        $('#result1 .cityImage p').text(cityNameOne);
+        $('#result2 .cityImage p').text(cityNameTwo);
+        getHousePricessApiData(cityCodeOne,filterData2);
+        getHousePricessApiData(cityCodeTwo,filterData2);
+        getCostOfLivingApiData(cityNameOne,filterData);
+        getCostOfLivingApiData(cityNameTwo,filterData);
+        let temp=dataset1[1];
+        dataset1[1]=dataset2[0];
+        dataset2[0]=temp;
+        showGraph(dataset1,dataset2);
+        showPieChart([cityNameOne,cityNameTwo],detailsCostofLiving);
+    });
+};
+function showGraph(dataset1, dataset2 ) {
+    var barChartData = {
+        labels: ['Rent Prices','Cost of Living'],
+        datasets: [{
+            label: cityNameOne,
+            backgroundColor: ["#C21460","#99ff9e"],
+            data: dataset1
+        }, {
+            label: cityNameTwo,
+            backgroundColor: ["#C21460","#C21460"],
+            data: dataset2
+        }]
+    };
+    var ctx =$("#myChart");
+    ctx.height = 500;
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+};
+function showPieChart(cityName,dataArray){
+    var costParameter=["Meal Cost", "Apartment Cost", "Utilities cost"];
+    var fcv=[],k=0;
+    fcv[0]=[dataArray[0],dataArray[3]];
+    fcv[1]=[dataArray[1],dataArray[4]];
+    fcv[2]=[dataArray[2],dataArray[5]];
+    $.each(costParameter, function( index, value ) {
+        console.log(value);
+        var pieConfig = {
+            labels: cityName,
+            datasets: [{
+                label: "Dollars",
+                backgroundColor: ["#C21460", "#FB9902"],
+                data: fcv[k]
+            }]
+        };
+        var ctxpie = $("#pieChart"+k);
+        var mypie = new Chart(ctxpie, {
+            type: 'pie',
+            data: pieConfig,
+            options: {
+                title: {
+                    display: true,
+                    text: 'Cost of Living for : ' + value
+                }
+            }
+        });
+        k++;
+    });
+
+};
+$(formSubmission);
